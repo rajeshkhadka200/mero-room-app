83c8421749d72828385bae432c3dde034958eb17
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2022-04-15 23:09:52 +0545

    restore design

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -28,7 +28,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    backgroundColor: "#fff",
     backgroundColor: "#EEEEEE",
     marginTop: 150,
   },
